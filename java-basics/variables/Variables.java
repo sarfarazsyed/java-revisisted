941a9e2b1bbdeac5b0a5743e4bbc46215ee99e3f
Sarfaraz Ahammed <syedsarfarazahammed@gmail.com>
2021-03-20 01:57:38 +0530

    Variables in java

---
+++ java-basics/variables/Variables.java
@@ -0,0 +1,12 @@
+class Book {
+  int id; // 'id' is a instance variable which varies for every instance of book
+}
+
+class Variables {
+  static int count = 0; //'count' is a static or class variable and it's stays same across all instances of Variables class
+  public static void main(String[] args) { //'args' is parameter and holds values passed by caller
+
+    int localVariable = 0; // 'localVariable' is local variable which is temporary storage until method executes
+
+  }
+}
