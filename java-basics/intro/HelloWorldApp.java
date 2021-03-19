64c5df9b0b1c53abcf3730050397ec86c80bf934
Sarfaraz Ahammed <syedsarfarazahammed@gmail.com>
2021-03-20 01:07:04 +0530

    Introduction to Java

---
+++ java-basics/intro/HelloWorldApp.java
@@ -0,0 +1,12 @@
+class HelloWorldApp {
+  /** This is a main method which gets invoked at start of application.
+   *
+   * @param args this holds array of {@link String} representing commad line arguments
+   */
+  public static void main(String[] args) {
+    /*
+     *Below line prints "Hello World" string on console.
+     */
+    System.out.println("Hello World"); //This is an inline or single line comment
+  }
+}
